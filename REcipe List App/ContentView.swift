7fb86a6f49590a9c36f73cc1b0e08be1c9804f9f
Tom Dudziak <network@fcafire.com>
2021-09-03 14:22:51 -0700

    Initial Commit

---
+++ REcipe List App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  REcipe List App
+//
+//  Created by user202247 on 9/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ REcipe List App/REcipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  REcipe_List_AppApp.swift
+//  REcipe List App
+//
+//  Created by user202247 on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct REcipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
